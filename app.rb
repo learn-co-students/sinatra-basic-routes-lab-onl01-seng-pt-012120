711a80c05c0bbe1d46826ba354bf2c2fd937513b
Roberto Livi <robertomlivi@outlook.com>
2020-03-29 04:26:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Roberto"
+  end
+  
+  get '/hometown' do
+    "My hometown is Miami"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Through the Fire and Flames"
+  end
+  
 end
