f3e65da14bb70d8ed207d3e4d73e96c1172a97a5
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-04-29 18:49:12 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+		"My name is Java"
+  end
+	get '/hometown' do
+		"My hometown is Corona"
+  end
+	get '/favorite-song' do
+		"My favorite song is Resistance"
+  end
 end
